190dff457627bcb36f00d5ec459a4fde138cba8f
AnaComBia <anabeatrizetorres@gmail.com>
2022-06-16 14:03:41 -0300

    finaliza

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 
 const n1 = Number (prompt("Digite sua primeira nota: "));
+
+if(isNaN(n1)||
+   n1 < 0 || n1 > 10){
+
+    alert("por favor digite números entre 0 e 10");
+
+}else {
+    return n1;
+} 
+    
 const n2 = Number (prompt("Digite sua segunda nota: "));
+
+if(isNaN(n2)||
+   n2 < 0 || n2 > 10){
+
+    alert("por favor digite números entre 0 e 10");
+
+}
+
 const n3 = Number (prompt("Digite sua terceira nota: "));
+
+if(isNaN(n3)||
+   n3 < 0 || n3 > 10){
+
+    alert("por favor digite números entre 0 e 10");
+
+}
+
 const n4 = Number (prompt("Digite sua quarta nota: "));
-
-
-if(n1 == NaN || isNaN(n2) || isNaN(n3) || isNaN(n4) ||
-   n1 < 0 || n1 > 10 ||
-   n2 < 0 || n2 > 10 ||
-   n3 < 0 || n3 > 10 ||
+if(isNaN(n4)||
    n4 < 0 || n4 > 10){
 
     alert("por favor digite números entre 0 e 10");
 
-} else {
+}
+
+else {
 
     const media = (n1 + n2 + n3 + n4) / 4; 
 
@@ -27,5 +50,6 @@ if(n1 == NaN || isNaN(n2) || isNaN(n3) || isNaN(n4) ||
     }
 
     alert (`Sua média final foi: ${media} e o seu resultado é: ${resultado}`);
+
 
 }
